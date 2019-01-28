86a0d775b27e8ee3f4511e837f4d4cfc5a67f679
DSmyth7 <d-jsmyth@hotmail.co.uk>
2019-01-28 12:09:38 +0000

    Added functionality to return list of cities

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [Route("api/cities")]
+
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+    }
+}
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -15,6 +15,8 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +27,8 @@ namespace CityInfo.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+       
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
